803290a166c7fe9b7012bb420bbcf71f8fe838ee
DYSTOpyy <bcd.saltel@gmail.com>
2026-02-24 13:21:23 +0100

    fix(logging): update log file name from bot.log to http.log

--- src/logging.rs
+++ src/logging.rs
@@ -1,7 +1,7 @@
 pub fn init_logging() -> tracing_appender::non_blocking::WorkerGuard {
     // tracing_subscriber::fmt().with_target(false).init(); // affichage terminal
 
-    let file_appender = tracing_appender::rolling::daily("./logs", "bot.log");
+    let file_appender = tracing_appender::rolling::daily("./logs", "http.log");
     // Create a non-blocking appender with a background worker. We return the
     // WorkerGuard so the caller can keep it alive until shutdown; dropping
     // the guard will flush the background worker and ensure buffered logs are

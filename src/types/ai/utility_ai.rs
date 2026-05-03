b8808c3966b1e4705e3a6eb780afc9208cb748cd
DYSTOpyy <bcd.saltel@gmail.com>
2026-05-03 19:07:34 +0200

    feat: adding new variables to additionnal_information adn utility_ai_variables

--- src/types/ai/utility_ai.rs
+++ src/types/ai/utility_ai.rs
@@ -1,13 +1,15 @@
 pub struct UtilityAiVariables {
-    constanteAttack: f64,
-    constanteHeal: f64,
+    pub constante_attack: f64,
+    pub constante_heal: f64,
+    pub health_ratio: f64,
 }
 
 impl UtilityAiVariables {
     pub fn new() -> Self {
         Self {
-            constanteAttack: 0.5,
-            constanteHeal: 0.5,
+            constante_attack: 0.5,
+            constante_heal: 0.5,
+            health_ratio: 1.0,
         }
     }
 }
--- src/types/game/character_additionnal_info.rs
+++ src/types/game/character_additionnal_info.rs
@@ -3,19 +3,21 @@ use crate::types::ai::utility_ai::UtilityAiVariables;
 pub struct CharacterAdditionnalInfo {
     //TODO Ici on peut mettre des infos supplémentaires sur le personnage qui ne viennent pas de l'API, mais qui peuvent être utiles pour l'IA
     // Par exemple : les ressources à collecter, les monstres à farmer, les équipements à trouver...
-    targetID: String,
-    positionTargetX: i32,
-    positionTargetY: i32,
-    utilityAiVariables: UtilityAiVariables,
+    pub target_id: String,                        // cible actuelle à attaquer
+    pub position_target_x: i32,                   // position x de la cible actuelle
+    pub position_target_y: i32,                   // position y de la cible actuelle
+    pub utility_ai_variables: UtilityAiVariables, // variables utilisées pour le calcul de l'utilité dans l'Utility AI
+    pub priority_target: Option<String>, // cible manuelle prioritaire (exemple : si je dis manuellement de buter ça, ou si on a une quête qui demande de buter un monstre précis) IL FAUT QU'A UN MOMENT ça REDEVIENNE NONE, sinon on va toujours attaquer la même cible, genre quand la quête est finie
 }
 
 impl CharacterAdditionnalInfo {
     pub fn new() -> Self {
         Self {
-            targetID: String::new(),
-            positionTargetX: 0,
-            positionTargetY: 0,
-            utilityAiVariables: UtilityAiVariables::new(),
+            target_id: String::new(),
+            position_target_x: 0,
+            position_target_y: 0,
+            utility_ai_variables: UtilityAiVariables::new(),
+            priority_target: None,
         }
     }
 }
